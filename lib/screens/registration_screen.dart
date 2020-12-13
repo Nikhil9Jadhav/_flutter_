89ee7b75a52e3b1b7818be911d695a910c961628
NJadhav <nikhil.jadhav@morningstar.com>
2020-12-13 14:28:40 +0530

    Added Flexible tag to manage screen rendering and updated gitignore file

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -25,9 +25,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: 200.0,
-              child: Image.asset('images/logo.png'),
+            Flexible(
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
             ),
             SizedBox(
               height: 48.0,
